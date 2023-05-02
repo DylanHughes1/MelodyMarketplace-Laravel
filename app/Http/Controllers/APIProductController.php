3ab1a0e5048e0475fec441b68c1e91aa9c4eb10b
Dylan Hughes <dylan_21174@hotmail.com>
2023-05-01 22:04:46 -0300

    API Product controller + route

---
+++ app/Http/Controllers/APIProductController.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\Subcategory;
+use Exception;
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
+
+class APIProductController extends Controller
+{
+    public function index(){
+
+        $products = Product::orderBy('id', 'asc')->get();
+        $categories = Category::all();
+        $subcategories = Subcategory::all();
+        
+        return response()->json([
+            'products' => $products,
+            'categories' => $categories,
+            'subcategories' => $subcategories
+        ]);
+    }
+
+    public function create(){
+
+        $categories = Category::all();
+        $subcategories = Subcategory::all();
+        return response()->json([
+            'categories' => $categories,
+            'subcategories' => $subcategories,
+        ]);
+    }
+
+    public function store(Request $request){
+        
+        $request->validate([
+            'name' => 'required|max:255',
+            'price' => 'required|integer|gt:0',
+            'subcategory' => 'required|integer',
+            'image' => 'required|image|max:1000'
+        ]);
+
+        try{
+            $image = $request->file('image');
+            $uploadedFile = $image->storeOnCloudinary('/products');
+        }catch (Exception $e){
+            return response()->json([
+                'message' => "Ocurrió un error al almacenar la imagen\n"
+            ], 400);
+        }
+
+        $product = new Product();
+        $product->name = $request->get('name');
+        $product->price = $request->get('price');
+        $product->subcategory_id = $request->get('subcategory');
+        $product->hasStock = true;
+        $product->image_link = $uploadedFile->getPath();
+        $product->image_path = $uploadedFile->getPublicId();
+
+        $product->save();
+
+        return response()->json([
+            'message' => 'Product created successfully'
+        ], 201);
+    }
+
+    public function show(string $id)
+    {
+        $product = Product::find($id);
+        if($product == null)
+            return response()->json(['error' => 'Producto no encontrado'], 404);
+
+        $categories = Category::all();
+        $subcategories = Subcategory::all();
+
+        return response()->json([
+            'product' => $product,
+            'categories' => $categories,
+            'subcategories' => $subcategories
+        ]);
+    }
+    public function editImage(string $id)
+    {
+        $product = Product::find($id);
+        if($product == null)
+            return response()->json(['error' => 'Producto no encontrado'], 404);
+
+        if($product->hasStock)
+            $product->hasStock = false;
+        $product->save();
+
+        return response()->json([
+            'message' => 'Image edited successfully'
+        ], 201);
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $product = Product::find($id);
+        if($product == null)
+            return response()->json(['error' => 'Producto no encontrado'], 404);
+
+        $request->validate([
+            'name' => 'string|max:255',
+            'price' => 'nullable|numeric|gt:0',
+            'subcategory' => 'integer',
+        ]);
+
+        $product->name = $request->input('name');
+        $product->price = $request->input('price');
+        $product->subcategory_id = $request->input('subcategory');
+
+        if ($request->has('image')) {
+            $request->validate([
+                'image' => 'required|image|max:1000'
+            ]);
+
+            if($product->image_path != null){
+                Cloudinary::destroy($product->image_path);  
+            }
+
+            $image = $request->file('image');
+            $uploadedFile = $image->storeOnCloudinary('products');
+            $product->image_link = $uploadedFile->getSecurePath();
+            $product->image_path = $uploadedFile->getPublicId();
+        }
+
+        $product->save();
+
+        return response()->json([
+            'message' => 'Product updated successfully'
+        ], 201);
+    }
+
+    }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\APIProductController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,8 +17,12 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); */
+
+Route::apiResource('products', APIProductController::class);
+Route::put('/products/{id}/disable', [APIProductController::class, 'editImage']);
+Route::post('/products/create', [APIProductController::class, 'store']);
