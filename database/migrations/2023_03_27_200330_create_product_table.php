2928f09309aec76350dd80a4d20544fb1019ae21
Dylan Hughes <dylan_21174@hotmail.com>
2023-05-28 12:41:20 -0300

    Fix product migration

--- database/migrations/2023_03_27_200330_create_product_table.php
+++ database/migrations/2023_03_27_200330_create_product_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('image_link')->nullable();
             $table->string('image_path')->nullable();
-            $table->boolean('hasStock');
+            $table->boolean('hasStock')->nullable();
             $table->decimal('price', 10, 2);
 
             $table->unsignedBigInteger('subcategory_id')->nullable();
