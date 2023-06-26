87778bede5bbab0554b92e9fa6c91fd82026270c
Martin Maslein <tincho_m98@hotmail.com>
2023-06-26 17:55:20 -0300

    AuthController implement

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
-class AuthController extends Controller
-{
-    //
+class AuthController extends Controller {
+    // register a new user method
+    public function register(RegisterRequest $request) {
+
+        $data = $request->validated();
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['token' => $token], 200);
+    }
+
+    // login a user method
+
+    public function login(Request $request)
+    {
+        // Validate the login request
+        $credentials = $request->only('email', 'password');
+
+        // Attempt to authenticate the user
+        if (Auth::attempt($credentials)) {
+            $client = User::where('email', $credentials['email'])->first();
+            $token = $client->createToken('client-token')->plainTextToken;
+            return response()->json([
+                'token' => $token
+            ], 200);
+        } else {
+            // Authentication failed
+            return response()->json([
+                'message' => 'Authentication error'
+            ], 401);
+        }
+    }
+
+    // logout a user method
+    public function logout(Request $request){
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'message' => 'Logged out successfully!'
+        ], 200);
+    }
+
+    // get the authenticated user method
+    public function user(Request $request) {
+        return new UserResource($request->user());
+    }
 }
