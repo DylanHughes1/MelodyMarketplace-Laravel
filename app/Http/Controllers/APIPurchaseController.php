a5b793374ac9e6d0811f5c86b81cf2e3bdc6def0
Dylan Hughes <dylan_21174@hotmail.com>
2023-06-29 11:29:15 -0300

    Update composer and recafor API

--- app/Http/Controllers/APIPurchaseController.php
+++ app/Http/Controllers/APIPurchaseController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Log;
+use MercadoPago;
 
-// require_once 'vendor/autoload.php';
+require_once 'vendor/autoload.php';
 class APIPurchaseController extends Controller
 {
 
@@ -15,19 +15,19 @@ class APIPurchaseController extends Controller
 
 
         // \MercadoPago\SDK::setAccessToken(env("VITE_ACCESS_TOKEN"));
-        \MercadoPago\SDK::setAccessToken("TEST-8154182059068188-062013-688347d68cda673bb7b097a204801577-393519241");
+        MercadoPago\SDK::setAccessToken("TEST-8154182059068188-062013-688347d68cda673bb7b097a204801577-393519241");
 
 
         $contents = $request->json()->all();
 
-        $payment = new \MercadoPago\Payment();
+        $payment = new MercadoPago\Payment();
         $payment->transaction_amount = $contents['transaction_amount'];
         $payment->token = $contents['token'];
         $payment->installments = $contents['installments'];
         $payment->payment_method_id = $contents['payment_method_id'];
         $payment->issuer_id = $contents['issuer_id'];
 
-        $payer = new \MercadoPago\Payer();
+        $payer = new MercadoPago\Payer();
         $payer->email = $contents['payer']['email'];
         $payer->identification = array(
             "type" => $contents['payer']['identification']['type'],
